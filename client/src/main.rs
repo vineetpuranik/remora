479123d80f4c2657cb3d5eb2788eb7be36dd3d0e
Vineet Puranik <vnt.manu@gmail.com>
2026-04-06 23:05:17 -0700

    Adding a grpc client to invoke the policy service

---
+++ client/src/main.rs
@@ -0,0 +1,55 @@
+use proto::policy:: {
+    policy_service_client::PolicyServiceClient,
+    AccessRequest, TokenRequest,
+};
+
+
+#[tokio::main]
+async fn main() {
+    // construct the service client
+    let mut client = PolicyServiceClient::connect("http://127.0.0.1:50051")
+    .await
+    .unwrap_or_else(|err| {
+        eprintln!("ERROR: could not connect to control plane : {err}");
+        std::process::exit(1);
+    });
+
+    println!("User: {}", "bob".to_string());
+
+    // get the token for the user
+    let token_response = client
+    .issue_token(TokenRequest {
+        user_id: "bob".to_string(),
+    })
+    .await
+    .unwrap_or_else(|err| {
+        eprintln!("ERROR: Issue token failed : {err}");
+        std::process::exit(1);
+    });
+
+    
+    let token = token_response.into_inner().token;
+    println!("Token issued  : {token}");
+
+    // check access based on in the issued token
+    let access_response = client.check_access(AccessRequest {
+        user_id: "alice".to_string(),
+        resource: "api_internal".to_string(),
+        action: "read".to_string(),
+        token,
+    })
+    .await
+    .unwrap_or_else(|err| {
+        eprintln!("ERROR: Check Access failed : {err}");
+        std::process::exit(1);
+    });
+
+    let response = access_response.into_inner();
+    if response.allowed {
+        println!("ALLOW - {}", response.reason);
+    } else {
+        println!("DENY - {}", response.reason);
+    }
+
+    return;
+}
